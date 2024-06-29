20e4aa59fd7ef990da1a2da482cfed68efdfd277
jihoon0083 <jihoon0083@gmail.com>
2024-06-29 22:39:30 +0900

    20240629 - 김지훈 - 레이아웃 생성

---
+++ layouts/error.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>에러페이지 입니다.</h1>
+        <p>잘되야 겠습니다.</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+
+</style>
---
+++ pages/main.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>메인페이지 입니다.</h1>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+
+</style>
